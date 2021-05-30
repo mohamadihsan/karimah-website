6f56d2eb4228c0546850fe2cf8cbf374b7c9642d
Mohamad Ihsan <ihsan.nutech@gmail.com>
2021-05-30 21:35:52 +0700

    update

--- resources/views/layouts/_frontend/template.blade.php
+++ resources/views/layouts/_frontend/template.blade.php
@@ -44,20 +44,20 @@
     <!-- ========== OPTIMIZE SEO ========== -->
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" type="text/css">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/frontend/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/vendor/animate.css/animate.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/aos/aos.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/remixicon/remixicon.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet" type="text/css">
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet" type="text/css">
 </head>
 
 <body>
@@ -80,16 +80,16 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/frontend/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/aos/aos.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/glightbox/js/glightbox.min.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/php-email-form/validate.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/swiper/swiper-bundle.min.js') }}" type="application/javascript"></script>
+    <script src="{{ asset('assets/frontend/vendor/waypoints/noframework.waypoints.js') }}" type="application/javascript"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('assets/frontend/js/main.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/main.js') }}" type="application/javascript"></script>
 
 </body>
 
